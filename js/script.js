14f63505a5f76bd766d53dec76d1a892bca89a03
Shoaib Naseer <43854873+Shoaib-Naseer@users.noreply.github.com>
2023-02-25 23:08:03 +0500

    Mobile First website

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const menu = document.getElementById('menu');
+const hamburger = document.getElementById('hamburger');
+hamburger.addEventListener('click', function () {
+  menu.classList.toggle('show');
+});
